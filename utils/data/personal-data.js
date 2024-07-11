0d03b21c4ea27bfe57ed81fe35c812d25ca8318a
marcossolis07 <marcossolis023@gmail.com>
2024-07-11 10:10:08 -0300

    correcciones de acentos y acortado de texto, cambie el CV

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,3 +1,6 @@
 export const personalData = {
-    resume: "https://drive.google.com/file/d/178DuoZx5K-D4g5BWadozuZnGkWu5x6R1/view?usp=drive_link"
+    resume: "https://drive.google.com/file/d/1BL5qgSuWpsiCBGt78XjVrjHeWeqVMz62/view?usp=drive_link"
 }
+
+//CV VIEJO
+//"https://drive.google.com/file/d/178DuoZx5K-D4g5BWadozuZnGkWu5x6R1/view?usp=drive_link"
